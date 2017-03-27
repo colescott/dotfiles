bfd87c3337e8a8889f4bc98ec24c41df8f74b9d5
Cole Scott <colescottsf@gmail.com>
2017-03-27 16:52:12 -0700

    Add install script
    This script will copy make symlinks from ~/* to ./files/* for
    every file in ./files. It should work recursively but that is
    untested.

---
+++ install.sh
@@ -0,0 +1,31 @@
+set -e
+
+OS="`uname`"
+
+# Make backup folder
+echo "Making backup dir"
+mkdir -p ~/dot-backup
+
+# Install all Config Files
+echo "Linking Config Files"
+for file in $(find "files" -type f) 
+do
+    name=${file#files/}
+    if [ -e ~/$name ]; then
+        echo "Backing up $name"
+        cp ~/$name ~/dot-backup
+    fi
+
+    echo "Linking $name"
+    ln -sf $(pwd)/files/$name ~/$name
+done
+
+# Clean up backup dir if empty
+if [ "$(ls -A ~/dot-backup)" ]; then
+    echo "Backup at ~/dot-backup"
+else
+    echo "Removing empty backup dir"
+fi
+
+
+echo "Done linking all files"
