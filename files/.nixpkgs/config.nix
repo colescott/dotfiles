85477d94588bb0fc3475fa9a32ebb0a73f7b789f
Cole Scott <colescottsf@gmail.com>
2017-08-20 21:47:36 -0700

    Fully replace the bs with home-manager

--- files/.nixpkgs/config.nix
+++ files/.nixpkgs/config.nix
@@ -1,32 +1,3 @@
 {
   allowUnfree = true;
-
-  packageOverrides = pkgs_: with pkgs_; {
-
-    all = with pkgs; buildEnv {
-      name = "all";
-
-      paths = [
-        # Zsh stuff
-        fortune
-        cowsay
-        lolcat
-
-        # Basic apps
-        discord
-        google-chrome
-        vscode
-        slack
-        spotify
-
-        # Games
-        steam
-
-        # Utils
-        file
-        nox
-        openssl
-      ];
-    };
-  };
 }
